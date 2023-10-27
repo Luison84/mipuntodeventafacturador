bf3ab209d71a53ae3850259395101cb4f57ba5c5
Luison84 <luislozano.arica@gmail.com>
2023-10-26 19:51:33 -0500

    Update seguridad_modulos_perfiles.php

--- vistas/seguridad_modulos_perfiles.php
+++ vistas/seguridad_modulos_perfiles.php
@@ -489,9 +489,9 @@
                     }
                 }
             ],
-            // initComplete: function(settings, json){
-            //     $('#tbl_perfiles_asignar').DataTable().columns.adjust();
-            // },
+            initComplete: function(settings, json){
+                $('#tbl_perfiles_asignar').DataTable().columns.adjust();
+            },
             language: {
                 "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
             }
