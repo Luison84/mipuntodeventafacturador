53ef4c95bb0a5a5c38c3bdc867b1badd3cd319f3
Luison84 <luislozano.arica@gmail.com>
2023-10-26 22:48:02 -0500

    Update plantilla.php

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -109,7 +109,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> -->
 
     <!-- Bootstrap 4 -->
-    <!-- <script src="vistas/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+    <script src="vistas/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- ChartJS -->
     <script src="vistas/assets/plugins/chart.js/Chart.min.js"></script>
@@ -126,7 +126,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <!-- jquery UI -->
-    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js" ></script>
+    <!-- <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js" ></script> -->
 
     <script src="vistas/assets/plugins/jquery-ui/js/jquery-ui.js"></script>
     <!-- <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js" integrity="sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=" crossorigin="anonymous"></script> -->
