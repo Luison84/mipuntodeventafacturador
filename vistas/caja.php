29c852f750906a09754644eb2cef46280f91e20a
Luison84 <luislozano.arica@gmail.com>
2023-10-29 13:39:32 -0500

    Update caja.php

--- vistas/caja.php
+++ vistas/caja.php
@@ -1,3 +1,11 @@
+<?php
+
+    require "ajax/rutas.ajax.php";
+
+?>
+
+
+
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
@@ -1091,7 +1099,7 @@ M O D A L   C I E R R E   D E   C A J A
 
     function fnc_ImprimirArqueo($id_arqueo_caja) {
 
-        $ruta_proyecto = <?php echo Rutas::RutaProyecyo(); ?>
+        $ruta_proyecto = <?php echo Rutas::RutaProyecyo(); ?>;
         console.log("🚀 ~ file: caja.php:1095 ~ fnc_ImprimirArqueo ~ $ruta_proyecto:", $ruta_proyecto)
 
         window.open('https://tutorialesphperu.com/pos/vistas/imprimir_arqueo.php?id_arqueo_caja=' + $id_arqueo_caja,
