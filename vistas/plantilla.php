b2227a426543bd553fd59cc28900d656af11416f
Luison84 <luislozano.arica@gmail.com>
2023-10-29 11:08:02 -0500

    Update plantilla.php

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -9,7 +9,7 @@ if (isset($_GET["cerrar_sesion"]) && $_GET["cerrar_sesion"] == 1) {
 
     echo '
             <script>
-                window.location = "/";
+                window.location = "/pos";
             </script>        
         ';
 }
