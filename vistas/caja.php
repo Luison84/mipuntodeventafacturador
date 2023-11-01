5a9bcdfc37341ab80001633c30f4f3f76c7ddeba
Luison84 <luislozano.arica@gmail.com>
2023-10-31 20:53:01 -0500

    Update caja.php

--- vistas/caja.php
+++ vistas/caja.php
@@ -1090,6 +1090,7 @@ M O D A L   C I E R R E   D E   C A J A
     }
 
     function fnc_ImprimirArqueo($id_arqueo_caja) {
+        
 
         window.open('https://tutorialesphperu.com/pos/vistas/imprimir_arqueo.php?id_arqueo_caja=' + $id_arqueo_caja,
             "ModalPopUp",
