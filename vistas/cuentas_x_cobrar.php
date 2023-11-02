21562f596f62c847c483655e11153e19bb1dde58
Luison84 <luislozano.arica@gmail.com>
2023-11-01 20:28:08 -0500

    Create cuentas_x_cobrar.php

---
+++ vistas/cuentas_x_cobrar.php
@@ -0,0 +1,16 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h2 class="m-0 fw-bold">Cuentas por Cobrar</h2>
+            </div><!-- /.col -->
+            <div class="col-sm-6 d-none d-md-block">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/pos"></a>Inicio</li>
+                    <li class="breadcrumb-item active">Cuentas x Cobrar</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div><!-- /.content-header -->
