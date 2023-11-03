fa372a454caa3a7af01132fed1b2a523bb42d017
Luison84 <luislozano.arica@gmail.com>
2023-11-02 21:30:12 -0500

    Update cuentas_x_cobrar.php

--- vistas/cuentas_x_cobrar.php
+++ vistas/cuentas_x_cobrar.php
@@ -225,9 +225,10 @@ MODAL MOSTRAR DETALLE DE CUOTAS
                 url: 'ajax/ventas.ajax.php',
                 dataSrc: function(json) {
                     for (let index = 0; index < json.length; index++) {
-                        const element = array[index];
+                        const element = arjsonray[index];
                         console.log("🚀 ~ file: cuentas_x_cobrar.php:227 ~ fnc_CargarDataTableCuotas ~ json:", element)
                     }
+
                     
                     return json;
                 },
