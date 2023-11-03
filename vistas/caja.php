cf057da3df9e289d6c15c2756bddefbf5432f8ce
Luison84 <luislozano.arica@gmail.com>
2023-11-03 13:02:30 -0500

    Update caja.php

--- vistas/caja.php
+++ vistas/caja.php
@@ -884,6 +884,7 @@ M O D A L   C I E R R E   D E   C A J A
                     //     $("#tbl_movimientos_devoluciones").DataTable().ajax.reload;
                     // }
                     if (data.action == "edit") {
+                        alert("entro")
                         mensajeToast("success", "Se actualizó la Devolución")
                         fnc_ObtenerEstadoCajaPorDia();
                         $("#tbl_movimientos_devoluciones").DataTable().ajax.reload();
