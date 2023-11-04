651aaacb31e4a39e43a4536a24f367e5aee7845b
Luison84 <luislozano.arica@gmail.com>
2023-11-03 19:09:12 -0500

    Update venta_boleta.php

--- vistas/venta_boleta.php
+++ vistas/venta_boleta.php
@@ -526,7 +526,7 @@
 
 
     <!-- FILA PARA IMAGEN DEL GIF -->
-    <div style="position: absolute;" id="img_carga">
+    <div style="position: absolute;top: 0;background: lightgrey;width: 99%;height: 100%;align-items: center;z-index: 500;" id="img_carga">
         <img src="vistas/assets/imagenes/loading.gif">
     </div>
 
