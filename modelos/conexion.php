1c3bf9f145edd47dd5b8f4f8a251ee013739efb6
Luison84 <luislozano.arica@gmail.com>
2023-11-11 17:28:50 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,8 +4,8 @@ class Conexion{
 
     static public function conectar(){
         try {
-            // $conn = new PDO("mysql:host=localhost;dbname=tutoria3_mitiendaposfacturador","tutoria3_tutorialesphperu","Emilia1109$",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            $conn = new PDO("mysql:host=localhost;dbname=mitiendaposfacturador","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $conn = new PDO("mysql:host=localhost;dbname=tutoria3_mitiendaposfacturador","tutoria3_tutorialesphperu","Emilia1109$",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            // $conn = new PDO("mysql:host=localhost;dbname=mitiendaposfacturador","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             return $conn;
         }
         catch (PDOException $e) {
