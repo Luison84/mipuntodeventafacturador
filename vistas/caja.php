5cf25ddb0c57a6c0bcce90148371041352cbeef5
Luison84 <luislozano.arica@gmail.com>
2023-11-11 23:29:32 -0500

    Update caja.php

--- vistas/caja.php
+++ vistas/caja.php
@@ -1184,6 +1184,7 @@ M O D A L   C I E R R E   D E   C A J A
                 })
 
                 fnc_CargarDataTableDevoluciones();
+                fnc_CargarDataTableArqueosCaja();
 
             }
         })
