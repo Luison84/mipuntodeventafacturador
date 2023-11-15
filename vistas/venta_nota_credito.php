39c60b5e982d03b51771e59c66afa8305faee180
Luison84 <luislozano.arica@gmail.com>
2023-11-15 18:54:08 -0500

    Update venta_nota_credito.php

--- vistas/venta_nota_credito.php
+++ vistas/venta_nota_credito.php
@@ -525,25 +525,25 @@
             console.log("🚀 ~ file: venta_nota_credito.php:524 ~ fnc_RecuperarVenta ~ element:", element)
 
             $('#tbl_ListadoProductos').DataTable().row.add({
-                'id': element.item,
-                'codigo_producto': element.codigo_producto,
-                // 'descripcion': element.descripcion,
-                // 'id_tipo_igv': element.id_tipo_afectacion_igv,
-                // 'tipo_igv': '',
-                // 'unidad_medida': element.unidad,
-                // 'precio': element.precio_unitario,
-                // 'cantidad': '<input type="number" style="width:80px;" codigoProducto = "' +
-                //     element.cantidad +
-                //     '" class="form-control form-control-sm text-center iptCantidad rounded-pill p-0 m-0" value="1">',
-                // 'cantidad_final': element.cantidad,
-                // 'subtotal': parseFloat(element.precio_unitario * element.cantidad).toFixed(2),
-                // 'igv': parseFloat((element.precio_unitario * element.cantidad * 0.18)).toFixed(2),
-                // 'importe': parseFloat((element.precio_unitario * element.cantidad) * 1.18).toFixed(2),
-                // 'acciones': "<center>" +
-                //     "<span class='btnEliminarproducto text-danger px-1'style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Eliminar producto'> " +
-                //     "<i class='fas fa-trash fs-5'> </i> " +
-                //     "</span>" +
-                //     "</center>"
+                'id': '1',
+                'codigo_producto': '1',
+                'descripcion': '1',
+                'id_tipo_igv': '1',
+                'tipo_igv': '',
+                'unidad_medida': '1',
+                'precio': '1',
+                'cantidad': '<input type="number" style="width:80px;" codigoProducto = "' +
+                    '1' +
+                    '" class="form-control form-control-sm text-center iptCantidad rounded-pill p-0 m-0" value="1">',
+                'cantidad_final': '1',
+                'subtotal': parseFloat('1' * '1').toFixed(2),
+                'igv': parseFloat(('1' * '1' * 0.18)).toFixed(2),
+                'importe': parseFloat(('1' * '1') * 1.18).toFixed(2),
+                'acciones': "<center>" +
+                    "<span class='btnEliminarproducto text-danger px-1'style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Eliminar producto'> " +
+                    "<i class='fas fa-trash fs-5'> </i> " +
+                    "</span>" +
+                    "</center>"
             }).draw();
 
         }
