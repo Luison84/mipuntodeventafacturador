f67490a2f1b1e152a27432f6fcd4667bf7848c67
Luison84 <luislozano.arica@gmail.com>
2023-11-15 20:33:21 -0500

    Update venta_nota_credito.php

--- vistas/venta_nota_credito.php
+++ vistas/venta_nota_credito.php
@@ -433,7 +433,7 @@
         ====================================================================================== */
         $('#tbl_ListadoProductos tbody').on('change', '.iptCantidad', function() {
 
-            cantidad_actual = $(this)[0]['value'];
+            cantidad_actual = $(this)[0]['attributes']['codigoproducto']['value'];
             cod_producto_actual = $(this)[0]['attributes']['codigoproducto']['value'];
 
             if (cantidad_actual.length == 0 || cantidad_actual == 0) {
@@ -446,8 +446,10 @@
                 var data = row.data();
 
                 //OBTENER PRECIO DEL PRODUCTO
-                $precio_sin_igv = $('#tbl_ListadoProductos').DataTable().cell(index, 6).data()/1.18;
+                $precio_sin_igv = parseFloat($('#tbl_ListadoProductos').DataTable().cell(index, 6).data())/1.18;
+                console.log("🚀 ~ file: venta_nota_credito.php:450 ~ $ ~ precio_sin_igv:", precio_sin_igv)
                 $id_tipo_afectacion = $('#tbl_ListadoProductos').DataTable().cell(index, 3).data();
+                console.log("🚀 ~ file: venta_nota_credito.php:452 ~ $ ~ id_tipo_afectacion:", id_tipo_afectacion)
 
                 let $subtotal = 0;
                 let $factor_igv = 0;
