4513de7eab7e9837465f1b6cbbdf091973cde71e
Luison84 <luislozano.arica@gmail.com>
2023-11-17 09:59:24 -0500

    Update venta_nota_credito.php

--- vistas/venta_nota_credito.php
+++ vistas/venta_nota_credito.php
@@ -430,7 +430,7 @@
 
         $('#serie').on('change', function(e) {
 
-            console.log($('#serie').val());
+            console.log($("#serie option:selected" ).text());
 
             if($('#serie').val().substring(1,1) == 'F'){
                 CargarSelect('01', $("#tipo_comprobante_modificado"), "--Seleccionar--", "ajax/series.ajax.php", 'obtener_tipo_comprobante_nota_credito');
