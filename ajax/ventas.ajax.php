916413633a46d7e3907ac633e7693bd49d180c5d
Luison84 <luislozano.arica@gmail.com>
2023-11-19 21:31:39 -0500

    Update ventas.ajax.php

--- ajax/ventas.ajax.php
+++ ajax/ventas.ajax.php
@@ -407,7 +407,7 @@ if (isset($_POST["accion"])) {
                 /*****************************************************************************************
                 R E G I S T R A R   V E N T A   Y   D E T A L L E   E N   L A   B D
                  *****************************************************************************************/
-                var_dump('iniciar registro de NC');
+                var_dump($venta);
                 $id_venta = VentasModelo::mdlRegistrarNotaCredito($venta, $detalle_venta);
                 var_dump('id_venta : ' . $id_venta);
                 return;
