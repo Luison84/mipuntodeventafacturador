345ac9223020705cadf4b285e7c3c446ca041a82
LuisLozano <46362305+Luison84@users.noreply.github.com>
2023-12-11 18:13:47 -0500

    Update venta_nota_credito.php

--- vistas/venta_nota_credito.php
+++ vistas/venta_nota_credito.php
@@ -1,32 +1,18 @@
 <!-- Content Header (Page header) -->
 <div class="content-header pb-1">
-
     <div class="container-fluid">
-
         <div class="row mb-2">
-
             <div class="col-sm-6">
-
-                <h2 class="m-0 fw-bold">NOTA DE CRÉDITO</h2>
-
+                <h2 class="m-0 fw-bold">NOTA DE CRÉDITO LUIS</h2>
             </div><!-- /.col -->
-
             <div class="col-sm-6 d-none d-md-block">
-
                 <ol class="breadcrumb float-sm-right">
-
                     <li class="breadcrumb-item"><a href="/">Inicio</a></li>
-
                     <li class="breadcrumb-item active">Ventas / Nota de Crédito</li>
-
                 </ol>
-
             </div><!-- /.col -->
-
         </div><!-- /.row -->
-
     </div><!-- /.container-fluid -->
-
 </div>
 <!-- /.content-header -->
 
