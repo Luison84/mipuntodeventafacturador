56a0b1b3997e5f187fb252c4b4dd09efb4af25c4
LuisLozano <46362305+Luison84@users.noreply.github.com>
2023-12-11 18:15:23 -0500

    Update venta_nota_credito.php

--- vistas/venta_nota_credito.php
+++ vistas/venta_nota_credito.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h2 class="m-0 fw-bold">NOTA DE CRÉDITO LUIS</h2>
+                <h2 class="m-0 fw-bold">NOTA DE CRÉDITO</h2>
             </div><!-- /.col -->
             <div class="col-sm-6 d-none d-md-block">
                 <ol class="breadcrumb float-sm-right">
