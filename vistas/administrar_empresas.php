cbe26dea5e3a914a92e59257691ebcaf67c94f61
LuisLozano <46362305+Luison84@users.noreply.github.com>
2023-12-30 17:08:05 -0500

    Update administrar_empresas.php

--- vistas/administrar_empresas.php
+++ vistas/administrar_empresas.php
@@ -234,7 +234,7 @@
                                     <!-- PREVIEW IMAGEN -->
                                     <div class="col-12 col-lg-12">
                                         <label class="mb-0 ml-1 text-sm my-text-color"><i class="fas fa-image mr-1 my-text-color"></i>Vista Previa</label>
-                                        <div style="width: 155%; height: 155px;text-align: center;">
+                                        <div style="width: 155px; height: 155px;">
                                             <img id="previewImg" src="vistas/assets/imagenes/no_image.jpg" class="border border-secondary" style="object-fit: fill; width: 100%; height: 100%;" alt="">
                                         </div>
                                     </div>
@@ -599,7 +599,7 @@
 
         if (file) {
             alert("entro")
-            
+
             var reader = new FileReader();
 
             reader.onload = function() {
