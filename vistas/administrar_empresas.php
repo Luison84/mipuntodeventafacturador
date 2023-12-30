1f96c145abaf786a6624a31427f054e602ce7b47
LuisLozano <46362305+Luison84@users.noreply.github.com>
2023-12-30 17:38:39 -0500

    Update administrar_empresas.php

--- vistas/administrar_empresas.php
+++ vistas/administrar_empresas.php
@@ -225,8 +225,26 @@
                                         <div class="invalid-feedback">Seleccion Estado</div>
                                     </div>
 
-                                    <div class="col-12 col-lg-3 mb-2">
-                                    <label class="mb-0 ml-1 text-sm my-text-color"><i class="fas fa-image mr-1 my-text-color"></i>Empresa Principal?</label>
+                                    <div class="col-12 col-lg-2 mb-2">
+                                        <label class="mb-0 ml-1 text-sm my-text-color"><i class="fas fa-image mr-1 my-text-color"></i>Empresa Principal?</label>
+                                        <div class="form-group clearfix w-100 d-flex justify-content-start justify-content-lg-start my-0 ">
+                                            <div class="icheck-warning d-inline mx-2">
+                                                <input type="radio" id="rb-venta-envio" value="1" name="rb_generar_venta" checked="">
+                                                <label for="rb-venta-envio">
+                                                    Si
+                                                </label>
+                                            </div>
+                                            <div class="icheck-success d-inline mx-2">
+                                                <input type="radio" id="rb-venta" value="2" name="rb_generar_venta">
+                                                <label for="rb-venta">
+                                                    No
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 col-lg-2 mb-2">
+                                        <label class="mb-0 ml-1 text-sm my-text-color"><i class="fas fa-image mr-1 my-text-color"></i>Empresa por Defecto?</label>
                                         <div class="form-group clearfix w-100 d-flex justify-content-start justify-content-lg-start my-0 ">
                                             <div class="icheck-warning d-inline mx-2">
                                                 <input type="radio" id="rb-venta-envio" value="1" name="rb_generar_venta" checked="">
@@ -255,7 +273,7 @@
                                     </div> -->
 
                                     <!-- IMAGEN -->
-                                    <div class="col-12 col-lg-7 mb-2">
+                                    <div class="col-12 col-lg-6 mb-2">
                                         <label class="mb-0 ml-1 text-sm my-text-color"><i class="fas fa-image mr-1 my-text-color"></i>Seleccione logo</label>
                                         <input type="file" class="form-control" id="imagen" name="imagen" accept="image/*" onchange="previewFile(this)">
                                     </div>
@@ -622,7 +640,7 @@
 
     // PREVISUALIZAR LA IMAGEN
     function previewFile(input) {
-        
+
         var file = $("#imagen").get(0).files[0];
 
         if (file) {
