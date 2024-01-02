2b4aad3f288155bb28c9cef07190d247dd34c7a8
LuisLozano <46362305+Luison84@users.noreply.github.com>
2024-01-02 18:46:02 -0500

    Update aside.php

--- vistas/modulos/aside.php
+++ vistas/modulos/aside.php
@@ -145,7 +145,7 @@ $menuUsuario = UsuarioModelo::mdlObtenerMenuUsuario($_SESSION["usuario"]->id_usu
         formData.append('accion','obtener_empresa_principal');
 
         var response = SolicitudAjax("ajax/empresas.ajax.php","POST",formData);
-        console.log("🚀 ~ file: aside.php:148 ~ fnc_CargarLogoEmpresa ~ response:", response)
+        console.log("🚀 ~ file: aside.php:148 ~ fnc_CargarLogoEmpresa ~ response:", response.logo)
 
         $("#logo_sistema").attr("src", "vistas/assets/dist/img/logos_empresas/" + response.logo);
     }
