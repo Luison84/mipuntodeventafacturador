a6eb1cbe3d1ce64e7e2537414cfd44cf3bac648e
LuisLozano <46362305+Luison84@users.noreply.github.com>
2024-01-05 18:32:58 -0500

    Update impresion_factura_a4.php

--- ajax/impresion_factura_a4.php
+++ ajax/impresion_factura_a4.php
@@ -11739,12 +11739,12 @@
 
 <body>
 
-    <div class="container-fluid">
+    <div class="container">
 
         <div class="row">
             <div class="col-3">
                 <div class="w-100 h-100">
-                    <img src="../vistas/assets/dist/img/logos_empresas/"<?php echo $venta["logo"] ?> alt="">
+                    <img src="../vistas/assets/dist/img/logos_empresas/"<?php echo $venta["logo"] ?> alt="" style="width: 150px;height: 150px;">
                 </div>                
             </div>
             <div class="col-6">
