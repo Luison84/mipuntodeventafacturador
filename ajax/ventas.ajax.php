5c1f8dc93d084d5b868aa5aabb4798911a91ab5c
LuisLozano <46362305+Luison84@users.noreply.github.com>
2024-01-05 19:57:42 -0500

    include bootstrap

--- ajax/ventas.ajax.php
+++ ajax/ventas.ajax.php
@@ -982,6 +982,7 @@ if (isset($_GET["accion"])) {
 
             require "impresion_factura_a4.php";
 
+            $html = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
             $html = ob_get_clean();
 
             $dompdf = new Dompdf();
