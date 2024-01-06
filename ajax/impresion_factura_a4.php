0c145c6f39ac1d5e4006fc8579ce6beecc4917f6
LuisLozano <46362305+Luison84@users.noreply.github.com>
2024-01-05 20:40:11 -0500

    Update impresion_factura_a4.php

--- ajax/impresion_factura_a4.php
+++ ajax/impresion_factura_a4.php
@@ -11749,7 +11749,7 @@
 
         <body>
             <tr>
-                <td><img src="<?php echo $imagenBase64 ?>" width="150" alt=""></td>
+                <td><img src="<?php echo $imagenBase64 ?>" width="130" alt=""></td>
                 <td colspan="3" style="text-align: center;">
                     <span style="font-size: 30px;"><?php echo $venta["empresa"] ?></span> <br>
                     <span><?php echo $venta["direccion_empresa"] ?></span> <br>
