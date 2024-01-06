c255fd5f2e2582929a4964483c7b04a2c53d9d1d
LuisLozano <46362305+Luison84@users.noreply.github.com>
2024-01-05 23:19:24 -0500

    Update ventas.ajax.php

--- ajax/ventas.ajax.php
+++ ajax/ventas.ajax.php
@@ -977,13 +977,13 @@ if (isset($_GET["accion"])) {
 
         case 'generar_factura_a4':
 
-            require "../phpqrcode/qrlib.php";
+            require("../phpqrcode/qrlib.php");
 
             $venta = VentasModelo::mdlObtenerVentaPorIdFormatoA4($_GET["id_venta"]);
             $detalle_venta = VentasModelo::mdlObtenerDetalleVentaPorId($_GET["id_venta"]);
 
             $text_qr = $venta["ruc"] . " | " . $venta["id_tipo_comprobante"] . " | " . $venta["serie"] . " | " . $venta["correlativo"] . " | " . $venta["total_igv"] . " | " . $venta["importe_total"] . " | " . $venta["fecha_emision"] . " | " . $venta["id_tipo_documento"] . " | " . $venta["nro_documento"];
-            $ruta_qr = "https://tutorialesphperu.com/pos/fe/qr/formato_a4_qr.png";
+            $ruta_qr = "../fe/qr/" . "prueba_qr_2" . '.png';
 
             QRcode::png($text_qr, $ruta_qr, 'Q', 15, 0);
 
